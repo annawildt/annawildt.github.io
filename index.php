a7ada1f8da62c2f39eeb4b130a8cc27b523725e9
Anna Wildt <anna.wildt@hotmail.com>
2021-12-20 14:05:07 +0000

    Add menu with php include

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Anna Wildt Github Pages</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="/javascript/script.js"></script>
+<body>
+
+<h1>Anna Wildt's Github Pages</h1>
+<?php include "menu.php"; ?>
+<div class="center-bg">
+    <p>This page is for testing and is WIP</p> 
+</div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<div class="menu">
+    <a href="index.html">Home</a>
+    <a href="cv.html">CV</a>
+    <a href="about.html">About</a>
+    <a href="contact.html">Contact</a>
+</div>
+</html>
